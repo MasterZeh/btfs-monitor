f55b10a0220ab16e8d0c0d111de4c58fbf1f71ec
Zeh <85850180+MasterZeh@users.noreply.github.com>
2021-08-05 21:23:31 +1000

    Add files via upload
    Installer shell script version 0.1

---
+++ install-btfs-monitor.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+set -x 
+
+#Install sendmail and git
+sudo apt-get install -y sendemail git
+
+#Create folder
+sudo mkdir -p /opt/btfs
+
+#Set permissions
+sudo chmod 777 /opt/btfs
+
+#Create crontab
+crontab -l > cronjob
+echo "*/15 * * * * /opt/btfs/btfs-monitor/monitor.sh" >> cronjob
+crontab cronjob
+
+#Clone Repository
+cd /opt/btfs/
+git clone https://github.com/MasterZeh/btfs-monitor.git
